02aed520643974b8b3833ffd423dbe2d53751396
Stewart ilondanga <stilondanga@gmail.com>
2018-02-06 12:11:32 +0300

    add app/src/main/java/io/github/stewilondanga/scorecounter/MainActivity.java

--- app/src/main/java/io/github/stewilondanga/scorecounter/MainActivity.java
+++ app/src/main/java/io/github/stewilondanga/scorecounter/MainActivity.java
@@ -2,6 +2,7 @@ package io.github.stewilondanga.scorecounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(8);
+    }
+
+    public void addThreeForTeamA(View v) {
+        displayForTeamA(3);
+    }
+
+    public void addTwoForTeamA(View v) {
+        displayForTeamA(2);
+    }
+
+    public void addOneForTeamA(View v) {
+        displayForTeamA(1);
     }
 
     /**
