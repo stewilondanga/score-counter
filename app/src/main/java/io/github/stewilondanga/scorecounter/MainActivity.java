d2c84adc560f01982d6a649185f48b0ce93b294a
Stewart ilondanga <stilondanga@gmail.com>
2018-02-06 12:57:04 +0300

    add app/src/main/java/io/github/stewilondanga/scorecounter/MainActivity.java

--- app/src/main/java/io/github/stewilondanga/scorecounter/MainActivity.java
+++ app/src/main/java/io/github/stewilondanga/scorecounter/MainActivity.java
@@ -36,7 +36,8 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void addThreeForTeamA(View v) {
-        displayForTeamA(3);
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
     }
 
     /**
